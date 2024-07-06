0ef89dfb6b2f1de4fc7be57b09795d2874b2902e
ugpii <76032801+ugpii@users.noreply.github.com>
2024-07-06 16:11:19 +0800

    Update CHERWIN_TOOLS.py

--- CHERWIN_TOOLS.py
+++ CHERWIN_TOOLS.py
@@ -339,9 +339,9 @@ def RESTART_SCRIPT(RESTART_SCRIPT_NAME):
 def CHECK():
     global CHERWIN_SCRIPT_CONFIG
     print('>>>>>>>开始获取版本信息...')
-    baseurl = 'https://github.com/CHERWING/CHERWIN_SCRIPTS/raw/main/'
+    baseurl = 'https://github.com/ugpii/CHERWIN_SCRIPTS/raw/main/'
     TOOLS_NAME = 'CHERWIN_TOOLS.py'
-    server_script_url = f'https://github.com/CHERWING/CHERWIN_SCRIPTS/raw/main/{TOOLS_NAME}'
+    server_script_url = f'https://github.com/ugpii/CHERWIN_SCRIPTS/raw/main/{TOOLS_NAME}'
     try:
         response = requests.get(f'{baseurl}CHERWIN_SCRIPT_CONFIG.json', verify=False)
         response.encoding = 'utf-8'
@@ -500,7 +500,7 @@ def get_ip():
     
 def main(APP_NAME, local_script_name, ENV_NAME, local_version, need_invite=False):
     global APP_INFO, TIPS, TIPS_HTML
-    git_url = f'https://github.com/CHERWING/CHERWIN_SCRIPTS/raw/main/{local_script_name}'
+    git_url = f'https://github.com/ugpii/CHERWIN_SCRIPTS/raw/main/{local_script_name}'
     if CHECK():
         APP_INFO = CHERWIN_SCRIPT_CONFIG.get("APP_CONFIG", {}).get(ENV_NAME, {})
         # print(APP_INFO)
